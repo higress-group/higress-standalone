5aa5a7ae51d743a7571da3c4bdd1a5a8714ee13f
Kent Dong <ch3cho@qq.com>
2024-03-07 10:17:59 +0800

    feat: Support configuring routes with Gateway API (#61)
    * feat: Support configuring routes with Gateway API
    1. Fix the Gateway API CRDs support in the API server.
    2. Generate a basic GatewayClass and Gateway resource during preparation.
    3. Fix the XDS cache issue causing delayed push of Gateway API configuration updates.
    
    * Remove PILOT_ENABLE_XDS_CACHE=false since the bug has been fixed

--- src/apiserver/pkg/converter/gatewayapi.go
+++ src/apiserver/pkg/converter/gatewayapi.go
@@ -8,6 +8,32 @@ import (
 )
 
 func registerGatewayApiConverters(scheme *runtime.Scheme) {
+	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1alpha2.GatewayClassList{}, &gwapiv1beta1.GatewayClassList{},
+		func(a, b interface{}, scope conversion.Scope) error {
+			lb := b.(*gwapiv1beta1.GatewayClassList)
+			la := a.(*gwapiv1alpha2.GatewayClassList)
+			lb.Items = make([]gwapiv1beta1.GatewayClass, len(la.Items))
+			for i := range la.Items {
+				if err := scheme.Convert(&la.Items[i], &lb.Items[i], scope); err != nil {
+					return err
+				}
+			}
+			setApiVersion(b, gwapiv1beta1.GroupVersion)
+			return nil
+		})
+	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1beta1.GatewayClassList{}, &gwapiv1alpha2.GatewayClassList{},
+		func(a, b interface{}, scope conversion.Scope) error {
+			lb := b.(*gwapiv1alpha2.GatewayClassList)
+			la := a.(*gwapiv1beta1.GatewayClassList)
+			lb.Items = make([]gwapiv1alpha2.GatewayClass, len(la.Items))
+			for i := range la.Items {
+				if err := scheme.Convert(&la.Items[i], &lb.Items[i], scope); err != nil {
+					return err
+				}
+			}
+			setApiVersion(b, gwapiv1alpha2.GroupVersion)
+			return nil
+		})
 	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1alpha2.GatewayClass{}, &gwapiv1beta1.GatewayClass{},
 		func(a, b interface{}, scope conversion.Scope) error {
 			*(b.(*gwapiv1beta1.GatewayClass)) = gwapiv1beta1.GatewayClass(*a.(*gwapiv1alpha2.GatewayClass))
@@ -21,6 +47,32 @@ func registerGatewayApiConverters(scheme *runtime.Scheme) {
 			return nil
 		})
 
+	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1alpha2.GatewayList{}, &gwapiv1beta1.GatewayList{},
+		func(a, b interface{}, scope conversion.Scope) error {
+			lb := b.(*gwapiv1beta1.GatewayList)
+			la := a.(*gwapiv1alpha2.GatewayList)
+			lb.Items = make([]gwapiv1beta1.Gateway, len(la.Items))
+			for i := range la.Items {
+				if err := scheme.Convert(&la.Items[i], &lb.Items[i], scope); err != nil {
+					return err
+				}
+			}
+			setApiVersion(b, gwapiv1beta1.GroupVersion)
+			return nil
+		})
+	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1beta1.GatewayList{}, &gwapiv1alpha2.GatewayList{},
+		func(a, b interface{}, scope conversion.Scope) error {
+			lb := b.(*gwapiv1alpha2.GatewayList)
+			la := a.(*gwapiv1beta1.GatewayList)
+			lb.Items = make([]gwapiv1alpha2.Gateway, len(la.Items))
+			for i := range la.Items {
+				if err := scheme.Convert(&la.Items[i], &lb.Items[i], scope); err != nil {
+					return err
+				}
+			}
+			setApiVersion(b, gwapiv1alpha2.GroupVersion)
+			return nil
+		})
 	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1alpha2.Gateway{}, &gwapiv1beta1.Gateway{},
 		func(a, b interface{}, scope conversion.Scope) error {
 			*(b.(*gwapiv1beta1.Gateway)) = gwapiv1beta1.Gateway(*a.(*gwapiv1alpha2.Gateway))
@@ -34,6 +86,32 @@ func registerGatewayApiConverters(scheme *runtime.Scheme) {
 			return nil
 		})
 
+	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1alpha2.HTTPRouteList{}, &gwapiv1beta1.HTTPRouteList{},
+		func(a, b interface{}, scope conversion.Scope) error {
+			lb := b.(*gwapiv1beta1.HTTPRouteList)
+			la := a.(*gwapiv1alpha2.HTTPRouteList)
+			lb.Items = make([]gwapiv1beta1.HTTPRoute, len(la.Items))
+			for i := range la.Items {
+				if err := scheme.Convert(&la.Items[i], &lb.Items[i], scope); err != nil {
+					return err
+				}
+			}
+			setApiVersion(b, gwapiv1beta1.GroupVersion)
+			return nil
+		})
+	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1beta1.HTTPRouteList{}, &gwapiv1alpha2.HTTPRouteList{},
+		func(a, b interface{}, scope conversion.Scope) error {
+			lb := b.(*gwapiv1alpha2.HTTPRouteList)
+			la := a.(*gwapiv1beta1.HTTPRouteList)
+			lb.Items = make([]gwapiv1alpha2.HTTPRoute, len(la.Items))
+			for i := range la.Items {
+				if err := scheme.Convert(&la.Items[i], &lb.Items[i], scope); err != nil {
+					return err
+				}
+			}
+			setApiVersion(b, gwapiv1alpha2.GroupVersion)
+			return nil
+		})
 	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1alpha2.HTTPRoute{}, &gwapiv1beta1.HTTPRoute{},
 		func(a, b interface{}, scope conversion.Scope) error {
 			*(b.(*gwapiv1beta1.HTTPRoute)) = gwapiv1beta1.HTTPRoute(*a.(*gwapiv1alpha2.HTTPRoute))
@@ -47,6 +125,32 @@ func registerGatewayApiConverters(scheme *runtime.Scheme) {
 			return nil
 		})
 
+	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1alpha2.ReferenceGrantList{}, &gwapiv1beta1.ReferenceGrantList{},
+		func(a, b interface{}, scope conversion.Scope) error {
+			lb := b.(*gwapiv1beta1.ReferenceGrantList)
+			la := a.(*gwapiv1alpha2.ReferenceGrantList)
+			lb.Items = make([]gwapiv1beta1.ReferenceGrant, len(la.Items))
+			for i := range la.Items {
+				if err := scheme.Convert(&la.Items[i], &lb.Items[i], scope); err != nil {
+					return err
+				}
+			}
+			setApiVersion(b, gwapiv1beta1.GroupVersion)
+			return nil
+		})
+	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1beta1.ReferenceGrantList{}, &gwapiv1alpha2.ReferenceGrantList{},
+		func(a, b interface{}, scope conversion.Scope) error {
+			lb := b.(*gwapiv1alpha2.ReferenceGrantList)
+			la := a.(*gwapiv1beta1.ReferenceGrantList)
+			lb.Items = make([]gwapiv1alpha2.ReferenceGrant, len(la.Items))
+			for i := range la.Items {
+				if err := scheme.Convert(&la.Items[i], &lb.Items[i], scope); err != nil {
+					return err
+				}
+			}
+			setApiVersion(b, gwapiv1alpha2.GroupVersion)
+			return nil
+		})
 	_ = scheme.Converter().RegisterUntypedConversionFunc(&gwapiv1alpha2.ReferenceGrant{}, &gwapiv1beta1.ReferenceGrant{},
 		func(a, b interface{}, scope conversion.Scope) error {
 			*(b.(*gwapiv1beta1.ReferenceGrant)) = gwapiv1beta1.ReferenceGrant(*a.(*gwapiv1alpha2.ReferenceGrant))
