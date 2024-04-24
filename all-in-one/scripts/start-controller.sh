45193351097536ddc0a2f5b438b43e0c52db5b05
CH3CHO <ch3cho@qq.com>
2024-04-24 10:49:23 +0800

    feat: Enhance the port management features of all-in-one image 1. Remove Gateway API resources so Gateway won't listen port 80 by default. 2. Change the default HTTP and HTTPS ports the gateway listens to. 3. Allow changing HTTP and HTTPS ports using environment variables.

--- all-in-one/scripts/start-controller.sh
+++ all-in-one/scripts/start-controller.sh
@@ -5,6 +5,16 @@ ROOT=$(pwd)
 cd - >/dev/null
 source $ROOT/base.sh
 
+if [ -z "$GATEWAY_HTTP_PORT" ] || [[ ! "$GATEWAY_HTTP_PORT" =~ ^[0-9]+$ ]] || ((GATEWAY_HTTP_PORT < 1 || GATEWAY_HTTP_PORT > 65535)); then
+    GATEWAY_HTTP_PORT=8080
+fi
+if [ -z "$GATEWAY_HTTPS_PORT" ] || [[ ! "$GATEWAY_HTTPS_PORT" =~ ^[0-9]+$ ]] || ((GATEWAY_HTTPS_PORT < 1 || GATEWAY_HTTPS_PORT > 65535)); then
+    GATEWAY_HTTPS_PORT=8443
+fi
+
+echo "GATEWAY_HTTP_PORT=$GATEWAY_HTTP_PORT"
+echo "GATEWAY_HTTPS_PORT=$GATEWAY_HTTPS_PORT"
+
 waitForApiServer
 
 set -e
@@ -14,4 +24,6 @@ set -e
     --kubeconfig=/app/kubeconfig \
     --gatewaySelectorKey=higress \
     --gatewaySelectorValue=higress-system-higress-gateway \
+    --gatewayHttpPort=$GATEWAY_HTTP_PORT \
+    --gatewayHttpsPort=$GATEWAY_HTTPS_PORT \
     --ingressClass=
