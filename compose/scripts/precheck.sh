25f354856782697652df9964f4c51525c7263134
Kent Dong <ch3cho@qq.com>
2025-02-15 10:10:32 +0800

    feat: Delay most of the initialization work to the start-up stage and always regenerate internal certificates (#133)

--- compose/scripts/precheck.sh
+++ compose/scripts/precheck.sh
@@ -3,32 +3,62 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 VOLUMES_ROOT="/mnt/volumes"
+RSA_KEY_LENGTH=4096
+
+checkExitCode() {
+  # $1 message
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    echo ${1:-"  Command fails with $retVal"}
+    exit $retVal
+  fi
+}
 
 checkApiServer() {
   echo "Checking API server configurations..."
 
-  if [ ! -d "$VOLUMES_ROOT/api/" ]; then
-    echo "  The volume of api is missing."
-    exit -1 
-  fi
-  cd "$VOLUMES_ROOT/api"
+  mkdir -p $VOLUMES_ROOT/api && cd "$_"
 
-  if [ ! -f ca.key ] || [ ! -f ca.crt ]; then
-    echo "  CA certificate files of API server are missing."
-    exit -1 
-  fi
-  if [ ! -f server.key ] || [ ! -f server.crt ]; then
-    echo "  Server certificate files of API server are missing."
-    exit -1 
-  fi
-  if [ ! -f client.key ] || [ ! -f client.crt ]; then
-    echo "  Client certificate files of API server are missing."
-    exit -1 
-  fi
-  if [ ! -f $VOLUMES_ROOT/kube/config ]; then
-    echo "  The kubeconfig file to access API server is missing."
-    exit -1 
-  fi
+  echo "  Generating CA certificate..."
+  openssl req -nodes -new -x509 -days 36500 -keyout ca.key -out ca.crt -subj "/CN=higress-root-ca/O=higress" >/dev/null 2>&1
+  checkExitCode "  Generating CA certificate for API server fails with $?"
+
+  echo "  Generating server certificate..."
+  openssl req -out server.csr -new -newkey rsa:$RSA_KEY_LENGTH -nodes -keyout server.key -subj "/CN=higress-api-server/O=higress" >/dev/null 2>&1 &&
+    openssl x509 -req -days 36500 -in server.csr -CA ca.crt -CAkey ca.key -set_serial 01 -sha256 -out server.crt >/dev/null 2>&1
+  checkExitCode "  Generating server certificate fails with $?"
+
+  echo "  Generating client certificate..."
+  openssl req -out client.csr -new -newkey rsa:$RSA_KEY_LENGTH -nodes -keyout client.key -subj "/CN=higress/O=system:masters" >/dev/null 2>&1 &&
+    openssl x509 -req -days 36500 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 02 -sha256 -out client.crt >/dev/null 2>&1
+  checkExitCode "  Generating client certificate fails with $?"
+
+  CLIENT_CERT=$(cat client.crt | base64 -w 0)
+  CLIENT_KEY=$(cat client.key | base64 -w 0)
+
+  echo "  Generating kubeconfig..."
+  mkdir -p $VOLUMES_ROOT/kube
+  cat <<EOF >$VOLUMES_ROOT/kube/config
+apiVersion: v1
+kind: Config
+clusters:
+  - name: higress
+    cluster:
+      server: https://apiserver:8443
+      insecure-skip-tls-verify: true
+users:
+  - name: higress-admin
+    user:
+      client-certificate-data: ${CLIENT_CERT}
+      client-key-data: ${CLIENT_KEY}
+contexts:
+  - name: higress
+    context:
+      cluster: higress
+      user: higress-admin
+preferences: {}
+current-context: higress
+EOF
 }
 
 checkApiServer
