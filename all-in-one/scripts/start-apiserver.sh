15c3aaaaa279aa08863eb89be136f8dd3df3b09e
Kent Dong <ch3cho@qq.com>
2025-05-12 19:26:20 +0800

    feat: Support modifying mesh config files in all-in-one images (#157)

--- all-in-one/scripts/start-apiserver.sh
+++ all-in-one/scripts/start-apiserver.sh
@@ -22,4 +22,20 @@ if [ -n "$CONFIG_TEMPLATE" ]; then
     fi
 fi
 
+MESH_CONFIG_DIR='/etc/istio/config'
+mkdir -p $MESH_CONFIG_DIR
+HIGRESS_CONFIG_FILE="/data/configmaps/higress-config.yaml"
+MESH_CONFIG_FILES=$(yq '.data | keys | .[]' "$HIGRESS_CONFIG_FILE")
+if [ -z "$MESH_CONFIG_FILES" ]; then
+    echo "  Missing required files in higress-config ConfigMap."
+    exit -1
+fi
+IFS=$'\n'
+for MESH_CONFIG_FILE in $MESH_CONFIG_FILES; do
+    if [ -z "$MESH_CONFIG_FILE" -o "$MESH_CONFIG_FILE" == "higress" ]; then
+        continue
+    fi
+    yq ".data.$MESH_CONFIG_FILE" "$HIGRESS_CONFIG_FILE" > "$MESH_CONFIG_DIR/$MESH_CONFIG_FILE"
+done
+
 apiserver --bind-address 127.0.0.1 --secure-port 18443 --storage file --file-root-dir /data --cert-dir /tmp
